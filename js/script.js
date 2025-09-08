0a858d8676ab4959bad1aa8c9ae88a38de85af3a
Aleiaco02 <alessandroiacovelli83@gmail.com>
2025-09-08 18:59:21 +0200

    esercizio completo

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ let form = document.getElementById("answers-form");
 // dichiaro il bottone
 let mioBottone = document.querySelector(".btn");
 // dichiarazione messaggio
-let messaggio =document.getElementById("message");
+
 
 console.log(mioUl);
 
@@ -24,18 +24,28 @@ let conteggio = countdown(10);
 
 console.log(form);
 
-mioBottone.addEventListener('submit', controlloRisultato())
+form.addEventListener('submit', controlloRisultato);
 
-function controlloRisultato() {
-    for(let i = 0; i=form.length; i++){
-  
-        for(let j = 0; i=mioUl.length; i++){
+function controlloRisultato(event) {
+    event.preventDefault(); // blocca il comportamento di submit
+    let form = document.getElementById("answers-form");
+    let messaggio = document.getElementById("message");
+    let mioUl = document.querySelector("ul");
+    let arrayConfronto = [];
 
-        if (form[i] === mioUl[j]) {
-            messaggio.innerHTML= "hai indovinato " + [i] + "numeri";
+    // ciclo su tutti gli input del form
+    for (let i = 0; i < form.elements.length; i++) {
+        let inputValue = form.elements[i].value;
+
+        // ciclo su tutti i li della ul
+        for (let j = 0; j < mioUl.children.length; j++) {
+            if (inputValue === mioUl.children[j].textContent) {
+                arrayConfronto.push(inputValue);
+                break; // evita di contare più volte lo stesso numero
+            }
         }
-
-        }
-
     }
+
+    messaggio.innerHTML = "Hai indovinato " + arrayConfronto.length + " numeri";
 }
+
